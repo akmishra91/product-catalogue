580ed9595c0ec1e2e5c6c1496584866b45050c17
myworksongit <39307550+myworksongit@users.noreply.github.com>
2020-01-11 03:17:32 +0530

    Updates

---
+++ precache-manifest.9ed1131c7525e67d4c61db4509d621b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f90162e14bd0d244b126829b9d3d80c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3c5123057a23ea2b07bc",
+    "url": "/my-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1ea7ff8efbc1f3117955",
+    "url": "/my-app/static/css/main.f57d515b.chunk.css"
+  },
+  {
+    "revision": "3c5123057a23ea2b07bc",
+    "url": "/my-app/static/js/2.ebb0ff27.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/my-app/static/js/2.ebb0ff27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ea7ff8efbc1f3117955",
+    "url": "/my-app/static/js/main.89dddd8b.chunk.js"
+  },
+  {
+    "revision": "1d0d1868f76663eec549",
+    "url": "/my-app/static/js/runtime-main.467216da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7e8749ae4af777340f1e5a90ce2ea49d",
+    "url": "/my-app/static/media/organicsattva.7e8749ae.png"
+  }
+]);
